bb959033a260fe0cac86d7d3ea618e6ee7a29b2b
Jaromir Obr <jaromir.obr@gmail.com>
2021-06-13 21:47:02 +0200

    Added an example with rectangle struct

---
+++ src/main.rs
@@ -0,0 +1,114 @@
+// Solution with struct method
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn get_area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn set_area(&mut self, rect: Rectangle) {
+        self.width = rect.width;
+        self.height = rect.height;
+    }
+
+    fn can_hold(&self, other: Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main() {
+    let mut rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.get_area()
+    );
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(rect3));
+    rect1.set_area(Rectangle {
+        width: 10,
+        height: 5,
+    });
+
+    println!(
+        "The changed area of the rectangle is {} square pixels.",
+        rect1.get_area()
+    );
+}
+
+// -------------------------------------------------------------
+
+//  Solution with struct
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+
+//     let rect_ref = &rect1;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(&rect1)
+//     );
+
+//     println!("rect1 is {:#?}", rect_ref);
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+// -------------------------------------------------------------
+
+// // Solution with tuple
+// fn main() {
+//     let rect1 = (30, 50);
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(rect1)
+//     );
+// }
+
+// fn area(dimensions: (u32, u32)) -> u32 {
+//     dimensions.0 * dimensions.1
+// }
+
+// -------------------------------------------------------------
+
+// // Solution with variables
+// fn main() {
+//     let width1 = 30;
+//     let height1 = 50;
+
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         area(width1, height1)
+//     );
+// }
+
+// fn area(width: u32, height: u32) -> u32 {
+//     width * height
+// }
